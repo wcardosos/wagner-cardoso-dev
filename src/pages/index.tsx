32eaaaea092086293b31c8b987aece3af42da662
Wagner Cardoso <wagnerdev01@gmail.com>
2022-07-04 20:21:36 -0300

    feat(components/footer) Add footer component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,13 +6,13 @@ import {
   Button,
   Center,
   Heading,
-  HStack,
   Image,
   Link,
   SimpleGrid,
   Text,
   VStack,
 } from '@chakra-ui/react';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
@@ -61,40 +61,7 @@ export default function Home() {
             </VStack>
           </Center>
         </SimpleGrid>
-        <Center py={['8', '8', '0']}>
-          <HStack spacing={['4', '8']}>
-            <Link
-              href="mailto:wagnerdev01@gmail.com"
-              target="_blank"
-            >
-              <Image
-                src="/assets/img/gmail-icon.png"
-                alt="Contato Gmail"
-                h={['6', '8']}
-              />
-            </Link>
-            <Link
-              href="https://linkedin.com/in/wagner-cardoso-dev"
-              target="_blank"
-            >
-              <Image
-                src="/assets/img/linkedin-icon.png"
-                alt="Contato LinkedIn"
-                h={['6', '8']}
-              />
-            </Link>
-            <Link
-              href="https://github.com/wcardosos"
-              target="_blank"
-            >
-              <Image
-                src="/assets/img/github-icon.png"
-                alt="Contato Github"
-                h={['6', '8']}
-              />
-            </Link>
-          </HStack>
-        </Center>
+        <Footer />
       </Box>
     </>
   );
