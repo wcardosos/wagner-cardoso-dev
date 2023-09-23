df97658ed5de07af970b0f8241e45872b3428532
Wagner Cardoso <wagnerdev01@gmail.com>
2023-09-22 21:54:59 -0300

    Add link to home on logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 export default function Header() {
   return (
     <header className="flex justify-between">
-      <img className="w-12 h-12 lg:w-16 lg:h-16" src="/assets/logo.webp" />
+      <a href="/">
+        <img className="w-12 h-12 lg:w-16 lg:h-16" src="/assets/logo.webp" />
+      </a>
     </header>
   );
 }
