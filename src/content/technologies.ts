01801760d6f8afaca3dd862c2b262d724fe0764f
Wagner Cardoso <wagnerdev01@gmail.com>
2024-01-29 22:20:38 -0300

    Add technologies enum's

---
+++ src/content/technologies.ts
@@ -0,0 +1,49 @@
+export enum Languages {
+  JAVASCRIPT = 'JavaScript',
+  TYPESCRIPT = 'TypeScript',
+  PYTHON = 'Python',
+  RUBY = 'Ruby'
+}
+
+export enum Frontend {
+  HTML = 'HTML',
+  CSS = 'CSS',
+  TAILWIND = 'Tailwind CSS',
+  REACTJS = 'React.js',
+  VUEJS = 'Vue.js',
+  NEXTJS = 'Next.js'
+}
+
+export enum Backend {
+  NODEJS = 'Node.js',
+  EXPRESS = 'Express',
+  NESTJS = 'Nest.js',
+  DJANGO = 'Django',
+  FLASK = 'Flask',
+  FAST_API = 'FastAPI',
+  RUBY_ON_RAILS = 'Ruby on Rails'
+}
+
+export enum Database {
+  POSTGRESQL = 'PostgreSQL',
+  MYSQL = 'MySQL',
+  MONGODB = 'MongoDB',
+  DYNAMODB = 'DynamoDB'
+}
+
+export enum Cloud {
+  AWS = 'AWS',
+  GCP = 'CGP',
+  VERCEL = 'Vercel'
+}
+
+export enum Utilities {
+  DOCKER = 'Docker',
+  GIT = 'Git',
+  JIRA = 'Jira'
+}
+
+export enum Agile {
+  SCRUM = 'Scrum',
+  KANBAN = 'Kanban'
+}
